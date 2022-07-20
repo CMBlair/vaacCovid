4d8a3a9131232a32d31c7aea327d41b7bbad2302
Akiem Watley <akiemiq4g@icloud.com>
2022-07-20 17:12:34 -0400

    Work done on July 20, top 5 deadly cities by rural and non-rural, building function in testing, main runs.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,9 +2,9 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
-import org.apache.spark.sql.functions.{col, expr, last}
+import org.apache.spark.sql.functions.{col, desc, expr, last}
 
-object Main {
+object Main{
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.OFF)
     Logger.getLogger("akka").setLevel(Level.OFF)
@@ -16,19 +16,43 @@ object Main {
       .config("spark.master", "local")
       .config("spark.eventLog.enabled", false)
       .getOrCreate()
+
+    // Please remember to update hdfs location path
     val df = spark.read.format("csv").option("header", "true").load("hdfs://localhost:9000/user/akiem/time_series_covid_19_deaths_US.csv")
-    val header = df.select("Admin2", "Province_State", "Population")
+
 
     //For Jul 1, 2020 - Jul 31, 2020
-    val startDay = 172
+    //Setup variables
+    val URBANPOPCRITERIA = 50000
+    val startDay = 173
     val endDay = 204
-    val testMthDF = df.select((df.columns.slice(startDay, endDay).map(m=>col(m))):_*)
-    val test = testMthDF.withColumn("Total", ((col(testMthDF.columns(31)) - col(testMthDF.columns(1)))))
-    //header.join(test, "*").show()
-    header.show()
-    testMthDF.show()
-    test.show()
 
+
+    //**************Return to renaming header columns***********
+
+    val header = List("Admin2", "Province_State", "Population", "UID")
+    val testList = header ++ (df.columns.slice(startDay,endDay).toList)
+
+
+
+
+    val testMthDF = df.select(testList.map(m=>col(m)):_*)
+    val test = testMthDF.withColumn("Total", ((col(testMthDF.columns(34)) - col(testMthDF.columns(4)))))
+
+    //header.show()
+    //testMthDF.show()
+    //test.show()
+
+    // Top 5 Most Deadly Cities from Urban Areas
+    test.select("*")
+      .where(col("Population").>=(URBANPOPCRITERIA))
+      .sort(desc("Total")).show(5)
+
+    // Top 5 Most Deadly Cities from Non-Urban Areas
+    test.select("*")
+      .where(col("Population").<(URBANPOPCRITERIA))
+      .where(col("Population").>(0))
+      .sort(desc("Total")).show(5)
 
 
     spark.stop()
--- src/main/scala/testing.scala
+++ src/main/scala/testing.scala
@@ -2,7 +2,7 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.dsl.expressions.StringToAttributeConversionHelper
-import org.apache.spark.sql.functions.{col, expr, last}
+import org.apache.spark.sql.functions.{col, desc, expr, last}
 
 object testing {
   def main(args: Array[String]): Unit = {
@@ -16,18 +16,45 @@ object testing {
         .config("spark.master", "local")
         .config("spark.eventLog.enabled", false)
         .getOrCreate()
+
+    // Please remember to update hdfs location path
     val df = spark.read.format("csv").option("header", "true").load("hdfs://localhost:9000/user/akiem/time_series_covid_19_deaths_US.csv")
-    val header = df.select("Admin2", "Province_State", "Population")
+
 
     //For Jul 1, 2020 - Jul 31, 2020
+    //Setup variables for framing a month by index
+    val URBANPOPCRITERIA = 50000
     val startDay = 173
     val endDay = 204
-    val testMthDF = df.select((df.columns.slice(startDay, endDay).map(m=>col(m))):_*)
-    val test = testMthDF.withColumn("Total", ((col(testMthDF.columns(31)) - col(testMthDF.columns(1)))))
-    //header.join(test, "*").show()
-    header.show()
-    testMthDF.show()
-    test.show()
+    val header = List("Admin2", "Province_State", "Population", "UID")
+    val testList = header ++ (df.columns.slice(startDay,endDay).toList)
+
+    def findDeadliestCity(byMonth: String): Unit = {
+
+
+      val testMthDF = df.select(testList.map(m=>col(m)):_*)
+      val test = testMthDF.withColumn("Total", ((col(testMthDF.columns(34)) - col(testMthDF.columns(4)))))
+
+      // Top 5 Most Deadly Cities from Urban Areas
+      test.select("*")
+        .where(col("Population").>=(URBANPOPCRITERIA))
+        .sort(desc("Total")).show(5)
+
+      // Top 5 Most Deadly Cities from Non-Urban Areas
+      test.select("*")
+        .where(col("Population").<(URBANPOPCRITERIA))
+        .where(col("Population").>(0))
+        .sort(desc("Total")).show(5)
+
+    }
+
+
+
+
+//  Just some testing print outs can remove ************************
+//    header.show()
+//    testMthDF.show()
+//    test.show()
 
 
 
